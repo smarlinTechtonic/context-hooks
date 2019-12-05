b71ccc7504f55d6507c3bdb1bf280fe2dfcb1dd7
Sonali Marlin <sonali.marlin@techtonic.com>
2019-12-05 09:05:39 -0700

    add localstorage to project

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -3,15 +3,24 @@
 // Now instead of passing them down to all children, we pass down ONE function, the dispatch function.
 // We're also starting off in this case with state (movies) set to an empty array.
 // uuid is moved to the movieReducer instead
+// we will use localStorage to store movies. So the useEffct hook will allow us to update localStorage whenwever the books state changes
 
-import React, { createContext, useReducer } from 'react';
+
+import React, { createContext, useReducer, useEffect } from 'react';
 // import uuid from 'uuid/v1';
 import { movieReducer } from '../reducers/movieReducer';
 
 export const MovieContext = createContext();
 
 const MovieContextProvider = (props) => {
-    const [movies, dispatch] = useReducer(movieReducer, []);
+    const [movies, dispatch] = useReducer(movieReducer, [], ()=> {
+        const localData = localStorage.getItem('movies');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('movies', JSON.stringify(movies))
+    }, [movies]);
     
     // const [movies, setMovies] = useState([
     //     {title: "Game Changers", id: 1},
